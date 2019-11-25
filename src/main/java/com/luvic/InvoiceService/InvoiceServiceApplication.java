41ea95bb5f292ea2895fcf962d835876243149b6
dsalas <dsalas@snaptechnology.net>
2019-11-25 07:49:30 -0600

    added local time configuration

--- src/main/java/com/luvic/InvoiceService/InvoiceServiceApplication.java
+++ src/main/java/com/luvic/InvoiceService/InvoiceServiceApplication.java
@@ -3,11 +3,24 @@ package com.luvic.InvoiceService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class InvoiceServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InvoiceServiceApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init()
+	{
+		// Setting Spring Boot SetTimeZone
+		TimeZone.setDefault( TimeZone.getTimeZone( "CST" ) );
+
+		System.out.println( "Date in CST: " + new Date().toString() );
 	}
 
 }
