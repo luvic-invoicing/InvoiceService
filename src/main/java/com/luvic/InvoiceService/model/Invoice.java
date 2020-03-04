d54740a9d52be66e987992a4e4ed2b6c244c53ed
dsalas <dsalas@snaptechnology.net>
2020-03-04 12:46:54 -0600

    test other charges

--- src/main/java/com/luvic/InvoiceService/model/Invoice.java
+++ src/main/java/com/luvic/InvoiceService/model/Invoice.java
@@ -11,291 +11,368 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.luvic.InvoiceService.enumerations.DocumentType;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import javax.persistence.*;
 
 /**
- *
  * @author Luis Fernando Arce
  */
 @Entity
-@Table(  schema = "luvictechlvd" )
-public class Invoice { //TODO: Add Validations to TotalAmounts - According to Hacienda - CALL ANOTHER SERVICE FOR THAT???
-    
+@Table( schema = "luvictechlvd" )
+public class Invoice
+{ //TODO: Add Validations to TotalAmounts - According to Hacienda - CALL ANOTHER SERVICE FOR THAT???
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
     private int id;
 
     private int secuencia;
-    
+
     private String idErp;
+
     @Enumerated
     private Status status; //TODO: Verfiy status acording to Database?
-    
+
     private String clientName;
+
     private String clientEmail;
+
     private String clientId;
+
     private String clientIdType; //01 Cédula Física, 02 Cédula Jurídica, 03 DIMEX, 04 NITE
+
     private double totalSell;
+
     private double totalDiscount;
+
     private double totalNetSell;
+
     private double totalTaxes;
+
     private double totalBill;
+
     private double totalSellExempt;
-    @JsonInclude(Include.NON_NULL)
+
+    @JsonInclude( Include.NON_NULL )
     private String codeSellCondition;
+
     private String codePaidMethod1;
-    @JsonInclude(Include.NON_NULL)
+
+    @JsonInclude( Include.NON_NULL )
     private String codePaidMethod2;
-    @JsonInclude(Include.NON_NULL)
+
+    @JsonInclude( Include.NON_NULL )
     private String codePaidMethod3;
-    @JsonInclude(Include.NON_NULL)
+
+    @JsonInclude( Include.NON_NULL )
     private String codePaidMethod4;
-    @JsonInclude(Include.NON_NULL)
+
+    @JsonInclude( Include.NON_NULL )
     private Date authorizationDate;
-    @JsonInclude(Include.NON_NULL)
+
+    @JsonInclude( Include.NON_NULL )
     private Date registryDate;
+
     private String fiscalConsecutive;
+
     private String billKey;
+
     private boolean isInvoice;
+
     private String customerId;
+
     private String code;
+
     private Double exchange;
+
     private String source;
+
     private Integer supplierId;
+
     private String applicationType;
 
     @Enumerated
     private DocumentType documentType;
-    
+
     @JsonManagedReference
-    @OneToMany(mappedBy="invoice", cascade = CascadeType.ALL)
+    @OneToMany( mappedBy = "invoice", cascade = CascadeType.ALL )
     private List<InvoiceLine> lines;
 
     @JsonManagedReference
     @OneToMany( mappedBy = "invoice", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER )
     @Fetch( value = FetchMode.SUBSELECT )
     private List<InvoiceOtherCharges> invoiceOtherCharges = new ArrayList<>();
-    
-    public Invoice() {
+
+    public Invoice()
+    {
         registryDate = new Date();
     }
-    
-    public String getClientName() {
+
+    public String getClientName()
+    {
         return clientName;
     }
 
-    public void setClientName(String clientName) {
+    public void setClientName( String clientName )
+    {
         this.clientName = clientName;
     }
 
-    public String getClientEmail() {
+    public String getClientEmail()
+    {
         return clientEmail;
     }
 
-    public void setClientEmail(String clientEmail) {
+    public void setClientEmail( String clientEmail )
+    {
         this.clientEmail = clientEmail;
     }
 
-    public String getClientId() {
+    public String getClientId()
+    {
         return clientId;
     }
 
-    public void setClientId(String clientId) {
+    public void setClientId( String clientId )
+    {
         this.clientId = clientId;
     }
 
-    public String getClientIdType() {
+    public String getClientIdType()
+    {
         return clientIdType;
     }
 
-    public void setClientIdType(String clientIdType) {
+    public void setClientIdType( String clientIdType )
+    {
         this.clientIdType = clientIdType;
     }
 
-    public double getTotalSell() {
+    public double getTotalSell()
+    {
         return totalSell;
     }
 
-    public void setTotalSell(double totalSell) {
+    public void setTotalSell( double totalSell )
+    {
         this.totalSell = totalSell;
     }
 
-    public double getTotalDiscount() {
+    public double getTotalDiscount()
+    {
         return totalDiscount;
     }
 
-    public void setTotalDiscount(double totalDiscount) {
+    public void setTotalDiscount( double totalDiscount )
+    {
         this.totalDiscount = totalDiscount;
     }
 
-    public double getTotalNetSell() {
+    public double getTotalNetSell()
+    {
         return totalNetSell;
     }
 
-    public void setTotalNetSell(double totalNetSell) {
+    public void setTotalNetSell( double totalNetSell )
+    {
         this.totalNetSell = totalNetSell;
     }
 
-    public double getTotalTaxes() {
+    public double getTotalTaxes()
+    {
         return totalTaxes;
     }
 
-    public void setTotalTaxes(double totalTaxes) {
+    public void setTotalTaxes( double totalTaxes )
+    {
         this.totalTaxes = totalTaxes;
     }
 
-    public double getTotalBill() {
+    public double getTotalBill()
+    {
         return totalBill;
     }
 
-    public void setTotalBill(double totalBill) {
+    public void setTotalBill( double totalBill )
+    {
         this.totalBill = totalBill;
     }
 
-    public double getTotalSellExempt() {
+    public double getTotalSellExempt()
+    {
         return totalSellExempt;
     }
 
-    public void setTotalSellExempt(double totalSellExempt) {
+    public void setTotalSellExempt( double totalSellExempt )
+    {
         this.totalSellExempt = totalSellExempt;
     }
 
-    public String getCodeSellCondition() {
+    public String getCodeSellCondition()
+    {
         return codeSellCondition;
     }
 
-    public void setCodeSellCondition(String codeSellCondition) {
+    public void setCodeSellCondition( String codeSellCondition )
+    {
         this.codeSellCondition = codeSellCondition;
     }
 
-    public String getCodePaidMethod1() {
+    public String getCodePaidMethod1()
+    {
         return codePaidMethod1;
     }
 
-    public void setCodePaidMethod1(String codePaidMethod1) {
+    public void setCodePaidMethod1( String codePaidMethod1 )
+    {
         this.codePaidMethod1 = codePaidMethod1;
     }
 
-    public String getCodePaidMethod2() {
+    public String getCodePaidMethod2()
+    {
         return codePaidMethod2;
     }
 
-    public void setCodePaidMethod2(String codePaidMethod2) {
+    public void setCodePaidMethod2( String codePaidMethod2 )
+    {
         this.codePaidMethod2 = codePaidMethod2;
     }
 
-    public String getCodePaidMethod3() {
+    public String getCodePaidMethod3()
+    {
         return codePaidMethod3;
     }
 
-    public void setCodePaidMethod3(String codePaidMethod3) {
+    public void setCodePaidMethod3( String codePaidMethod3 )
+    {
         this.codePaidMethod3 = codePaidMethod3;
     }
 
-    public String getCodePaidMethod4() {
+    public String getCodePaidMethod4()
+    {
         return codePaidMethod4;
     }
 
-    public void setCodePaidMethod4(String codePaidMethod4) {
+    public void setCodePaidMethod4( String codePaidMethod4 )
+    {
         this.codePaidMethod4 = codePaidMethod4;
     }
 
-    public Date getAuthorizationDate() {
+    public Date getAuthorizationDate()
+    {
         return authorizationDate;
     }
 
-    public void setAuthorizationDate(Date authorizationDate) {
+    public void setAuthorizationDate( Date authorizationDate )
+    {
         this.authorizationDate = authorizationDate;
     }
 
-    public Date getRegistryDate() {
+    public Date getRegistryDate()
+    {
         return registryDate;
     }
 
-    public void setRegistryDate(Date registryDate) {
+    public void setRegistryDate( Date registryDate )
+    {
         this.registryDate = registryDate;
     }
 
-    public String getFiscalConsecutive() {
+    public String getFiscalConsecutive()
+    {
         return fiscalConsecutive;
     }
 
-    public void setFiscalConsecutive(String fiscalConsecutive) {
+    public void setFiscalConsecutive( String fiscalConsecutive )
+    {
         this.fiscalConsecutive = fiscalConsecutive;
     }
 
-    public String getBillKey() {
+    public String getBillKey()
+    {
         return billKey;
     }
 
-    public void setBillKey(String billKey) {
+    public void setBillKey( String billKey )
+    {
         this.billKey = billKey;
     }
 
-    public boolean isIsInvoice() {
+    public boolean isIsInvoice()
+    {
         return isInvoice;
     }
 
-    public void setIsInvoice(boolean isInvoice) {
+    public void setIsInvoice( boolean isInvoice )
+    {
         this.isInvoice = isInvoice;
     }
 
-    public List<InvoiceLine> getLines() {
-        if(lines == null) {
+    public List<InvoiceLine> getLines()
+    {
+        if ( lines == null )
+        {
             lines = new ArrayList<InvoiceLine>();
         }
-        
+
         return lines;
     }
 
-    public void setLines(List<InvoiceLine> lines) {
+    public void setLines( List<InvoiceLine> lines )
+    {
         this.lines = lines;
     }
 
-    public List<InvoiceOtherCharges> getInvoiceOtherCharges() {
-        if(invoiceOtherCharges == null) {
-            invoiceOtherCharges = new ArrayList<InvoiceOtherCharges>();
-        }
-
+    public List<InvoiceOtherCharges> getInvoiceOtherCharges()
+    {
         return invoiceOtherCharges;
     }
 
-    public void setInvoiceOtherCharges(List<InvoiceOtherCharges> invoiceOtherCharges) {
+    public void setInvoiceOtherCharges( List<InvoiceOtherCharges> invoiceOtherCharges )
+    {
         this.invoiceOtherCharges = invoiceOtherCharges;
     }
 
-    public Status getStatus() {
+    public Status getStatus()
+    {
         return status;
     }
 
-    public void setStatus(Status status) {
+    public void setStatus( Status status )
+    {
         this.status = status;
     }
 
-    public int getId() {
+    public int getId()
+    {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId( int id )
+    {
         this.id = id;
     }
 
-    public int getSecuencia() {
+    public int getSecuencia()
+    {
         return secuencia;
     }
 
-    public void setSecuencia(int Secuencia) {
+    public void setSecuencia( int Secuencia )
+    {
         this.secuencia = Secuencia;
     }
 
-    public String getIdErp() {
+    public String getIdErp()
+    {
         return idErp;
     }
 
-    public void setIdErp(String idErp) {
+    public void setIdErp( String idErp )
+    {
         this.idErp = idErp;
     }
 
